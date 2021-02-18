6b897267423d77e76476751d66f00e89355e8314
betzii <bethelhemteshibelay@gmail.com>
2021-02-18 12:24:15 +0300

    destructiong

--- asset/js/03(Destructuring).js
+++ asset/js/03(Destructuring).js
@@ -14,21 +14,22 @@ document.addEventListener('DOMContentLoaded', () => {
 function arrayDestructuring() {
 
     // we have an array with the name and surname
-    let arr = ["Margaery", "Tyrell"]
+    let arr = ["Margaery", "Tyrell"];
 
     //1. Destructor this array to firstName and lastName
-
+    let [firstName, lastName] = arr;
     //2. Display it  [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_arr.innerHTML = `Hello , I am firstName lastName `
+    des_arr.innerHTML = `Hello , I am ${firstName} ${lastName} `;
 
 
     // we have an array with the name and surname
     let str = "Margaery Tyrell"
 
     //1. Destructor this string array to firstName and lastName by using split() method of string object
-
+    str = str.split(" ");
+    [firstName, lastName] = str;
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_string.innerHTML = `Hello , I am firstName lastName `
+    des_string.innerHTML = `Hello , I am ${firstName} ${lastName} `;
 
 
 
@@ -42,15 +43,15 @@ function objectDestructuring() {
     let person = { firstName: "John", lastName: "Smith" }
 
     //1. Destructor this object to firstName and lastName 
-
+    let { firstName, lastName } = person;
     //2. Display it [Replace it with ${firstName} ${lastName} once you have the variables]
-    des_obj1.innerHTML = `Hi , I am firstName lastName `
+    des_obj1.innerHTML = `Hi , I am ${firstName} ${lastName} `;
 
 
     //1. Destructor this object to fName and sName [different naming]
-
+    let { firstName: fName, lastName: sName } = person;
     //2. Display it [Replace it with ${Name} ${lName} once you have the variables]
-    des_obj2.innerHTML = `Hi , I am fName lName `
+    des_obj2.innerHTML = `Hi , I am ${fName} ${sName} `;
 
 
 }
@@ -58,13 +59,13 @@ function objectDestructuring() {
 function spreadSyntax() {
 
     //An array of Numbers to add 
-    const arr = [1, 2, 3, 4]
+    const arr = [1, 2, 3, 4];
 
     //1. Pass the array using spread syntax
-    const result = sum1();
+    const result = sum1(...arr);
 
     //Display it 
-    spread_syntax.innerHTML = `The Sum is : ${result} `
+    spread_syntax.innerHTML = `The Sum is : ${result} `;
 
 
 
@@ -85,7 +86,7 @@ function restSyntax() {
     const result = sum2(x1, x2, x3, x4, x5);
 
     //Display it 
-    rest_syntax.innerHTML = `The Sum is : ${result} `
+    rest_syntax.innerHTML = `The Sum is : ${result} `;
 
 
 
@@ -101,11 +102,11 @@ function sum1(x1, x2, x3, x4) {
 
 
 //3. Make this function to support rest, and add 5 numbers
-function sum2(x1, x2) {
+function sum2(x1, x2, ...rest) {
 
     //4. Function that add 5 numbers [Consider Adding the arr[0], arr[1], arr[2]]
 
-    return x1 + x2;
+    return x1 + x2 + rest[0] + rest[1] + rest[2];
 
 
 }
