36e30c2fff15cb922022d2c692a17a5f192c82a1
betzii <bethelhemteshibelay@gmail.com>
2021-02-18 12:05:53 +0300

    new loops (for in and for of) is done

--- asset/js/02(newLoops).js
+++ asset/js/02(newLoops).js
@@ -13,7 +13,9 @@ function forINLoop() {
     const student = { name: "Abebe", age: 21, year: "2dn year" }
 
     // 1. Create a for in loop that iterate over the student object
-
+    for (key in student) {
+        forInLoop.innerHTML += `<li class="list-group-item">${key} : ${student[key]}</li>`;
+    }
 
     // 2. Inside the loop paint the UI [Use the Format Given Below]
 
@@ -35,7 +37,9 @@ function forOFLoop() {
     const students = [{ name: "Abebe", age: 21, year: "2dn year" }, { name: "Kebede", age: 22, year: "2dn year" }, { name: "Abebech", age: 23, year: "2dn year" }]
 
     // 1. Create a for of loop that iterate over the students Array and displays the names of students 
-
+    for (element of students) {
+        forOfLoop.innerHTML += `<li class="list-group-item" >Name : ${element.name}</li>`;
+      }
 
     // 2. Inside the loop paint the UI [Use the Format Given Below]
 
