742d5c6d24c7588263bdfefc16f42e682e5e8f12
betzii <bethelhemteshibelay@gmail.com>
2021-02-18 15:00:18 +0300

    object oriented programming 3

--- asset/js/05(OOP3).js
+++ asset/js/05(OOP3).js
@@ -1,16 +1,23 @@
 // 1. Create a Person_ES6 Class 
-
+class Person_ES6 {
+    constructor(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
 
 
 //2. Create the constructor that initialize firstName and lastName
 
 
 //3. Define greeting() method that displays Full Name
-
+greeting() {
+    return `Hi ${this.firstName} ${this.lastName}`;
+  }
+}
 
 //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
+var Bran = new Person_ES6("Bran", "Stark");
 
 
 //5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "Bran.greeting()";
+Obj_C3.innerHTML = Bran.greeting();
